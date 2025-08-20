895d2203e2881934b1a02517d94b2fb864239f5f
Meriemchm <chamimeriem2302@gmail.com>
2025-08-20 17:11:22 +0100

    create popOverTrigger logique for switching stores

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,8 +1,23 @@
+import prismadb from "@/lib/prismadb";
+import React from "react";
 
+interface DashboardPageProps {
+  params: { storeId: string };
+}
 
-const DashboardPage = () => {
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
 
-  return <div>page</div>;
+  return (
+    <div>
+      {" "}
+      <p className="font-bold">Active Store : {store?.name}</p>{" "}
+    </div>
+  );
 };
 
 export default DashboardPage;
