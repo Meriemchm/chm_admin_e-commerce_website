3a2b3c6caa6fc282cfca94c7ba84e7849ac463a4
Meriemchm <chamimeriem2302@gmail.com>
2025-08-21 13:33:11 +0100

    add the settings page

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { SettingsForm } from "./components/settings-form";
 
-const SettingsPage = () => {
-  return <div>page</div>;
+interface SettingsPageProps {
+  params: {
+    storeId: string;
+  };
+}
+
+const SettingsPage: React.FC<SettingsPageProps> = async ({ params }) => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+
+  if (!store) {
+    redirect("/");
+  }
+
+  return (
+    <div className="flex-col">
+      {" "}
+      <div className="flex-1 space-x-4 ">
+        <SettingsForm initialData={store} />
+      </div>
+    </div>
+  );
 };
 
 export default SettingsPage;
