84c9e40c64d8d7d0c46c912a9ba66ec3cee64c42
Meriemchm <chamimeriem2302@gmail.com>
2025-08-22 18:01:10 +0100

    updating billboard pages to add and update billboard

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { useParams, useRouter } from "next/navigation";
+
+import { Plus } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Billboard } from "@/lib/generated/prisma";
+import { billboardColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+
+interface BillboardClientProps {
+  data: billboardColumn[];
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({data}) => {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={"Billboard (" + data.length + ")"}
+          description={"Manage billboard for your store"}
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+
+      <DataTable columns={columns} data={data}/>
+    </>
+  );
+};
