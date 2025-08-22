f9cd709c81e6b1be74ad888289cd39f00692a676
Meriemchm <chamimeriem2302@gmail.com>
2025-08-22 18:02:14 +0100

    add cloudinary domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "res.cloudinary.com", // Cloudinary for image uploads
+    ],
+  },
 };
 
 export default nextConfig;
