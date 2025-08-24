8f3b6345b02d732b093695ec7818c1a3c0c9c9a0
Meriemchm <chamimeriem2302@gmail.com>
2025-08-24 20:47:21 +0100

    add images to billborad table

--- app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
@@ -2,10 +2,12 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 import { CellAction } from "./cell-action";
+import Image from "next/image";
 
 export type billboardColumn = {
   id: string;
   label: string;
+  imageUrl: string;
   createdAt: string;
 };
 
@@ -15,11 +17,28 @@ export const columns: ColumnDef<billboardColumn>[] = [
     header: "Label",
   },
   {
+    accessorKey: "imageUrl",
+    header: "Image",
+    cell: ({ row }) => (
+      <div className="flex items-center">
+        <Image
+          src={row.original.imageUrl}
+          alt={row.original.label}
+          width={150}
+          height={150}
+          className="rounded-md object-cover"
+        />
+      </div>
+    ),
+  },
+  {
     accessorKey: "createdAt",
     header: "Date",
   },
   {
     id: "actions",
-    cell: ({row}) => <CellAction data={row.original} onEdit={()=>{}} onDelete={()=>{}} />,
+    cell: ({ row }) => (
+      <CellAction data={row.original} onEdit={() => {}} onDelete={() => {}} />
+    ),
   },
 ];
