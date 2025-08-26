4b31ae4a6c0abefba3914ad7c1f708a1fc0a6591
Meriemchm <chamimeriem2302@gmail.com>
2025-08-26 19:09:00 +0100

    add get api for store

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -73,3 +73,25 @@ export async function DELETE(
   }
 }
 
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    if (!params.storeId) {
+      return new NextResponse("Store ID is required", { status: 400 });
+    }
+
+    const store = await prismadb.store.findUnique({
+      where: {
+        id: params.storeId,
+      },
+    });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.error("[STORE_GET]", error);
+
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
