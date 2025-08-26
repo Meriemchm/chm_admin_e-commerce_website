8a51447222fb83f4bc03fe1d292507d95a0cb7ce
Meriemchm <chamimeriem2302@gmail.com>
2025-08-26 19:05:01 +0100

    create checkout api post

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,78 @@
+import Stripe from "stripe";
+
+import { headers } from "next/headers";
+
+import { stripe } from "@/lib/stripe";
+import { NextResponse } from "next/server";
+import prismadb from "@/lib/prismadb";
+
+export async function POST(req: Request) {
+  const body = await req.text();
+  const signature = (await headers()).get("Stripe-Signature");
+
+  if (!signature) {
+    return new NextResponse("Missing Stripe signature", { status: 400 });
+  }
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+  } catch (error: any) {
+    return new NextResponse(`Webhook Error: ${error.message} `, {
+      status: 400,
+    });
+  }
+
+  const session = event.data.object as Stripe.Checkout.Session;
+  const address = session?.customer_details?.address;
+
+  const addressComponents = [
+    address?.line1,
+    address?.line2,
+    address?.city,
+    address?.state,
+    address?.postal_code,
+    address?.country,
+  ];
+
+  const addressString = addressComponents.filter((c) => c !== null).join(", ");
+  //console.log("Webhook reçu :", event.type, event.data.object);
+
+
+  if (event.type === "checkout.session.completed") {
+    const order = await prismadb.order.update({
+      where: {
+        id: session?.metadata?.orderId,
+      },
+      data: {
+        isPaid: true,
+        address: addressString,
+        phone: session?.customer_details?.phone || "",
+      },
+
+      include: {
+        orderItems: true,
+      },
+    });
+
+    const productIds = order.orderItems.map((orderItem) => orderItem.productId);
+
+    await prismadb.product.updateMany({
+      where: {
+        id: {
+          in: [...productIds],
+        },
+      },
+      data: {
+        isArchived: true,
+      },
+    });
+  }
+
+  return new NextResponse(null, { status: 200 });
+}
